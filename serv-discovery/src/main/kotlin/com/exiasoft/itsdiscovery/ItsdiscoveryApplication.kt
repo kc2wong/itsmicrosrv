76cd358c502d8bd563394ec56fe55760ed608dfa
Kelvin Wong <kc2wong@hotmail.com>
2019-02-03 17:05:31 +0800

    Add discovery service

---
+++ serv-discovery/src/main/kotlin/com/exiasoft/itsdiscovery/ItsdiscoveryApplication.kt
@@ -0,0 +1,13 @@
+package com.exiasoft.itsdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ItsdiscoveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItsdiscoveryApplication>(*args)
+}
